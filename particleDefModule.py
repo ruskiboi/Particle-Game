0c56e29f3ed5e47a6f07cf5768a80095e3022c92
ruskiboi <63880257+ruskiboi@users.noreply.github.com>
2022-11-04 10:49:23 -0500

    commit 2

---
+++ particleDefModule.py
@@ -0,0 +1,11 @@
+sand_particle = {
+    "particleName": "sand",
+    "particle_ID": "0000",
+    "density": 1.63,
+    "strength": 0.25,
+    "stateOfMatter": "solid",
+    "canBurn": False,
+    "canMelt": True,
+    "canFloat": False,
+    "isLooseParticle": True
+}
