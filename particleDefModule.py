f0e9155b3e65a47672be40d1974d7738422d0562
ruskiboi <63880257+ruskiboi@users.noreply.github.com>
2022-11-08 14:17:47 -0600

    3rd commit

--- particleDefModule.py
+++ particleDefModule.py
@@ -1,11 +1,26 @@
+#The strength attribute is the yeild strength in psi, divided by 100
 sand_particle = {
     "particleName": "sand",
     "particle_ID": "0000",
     "density": 1.63,
     "strength": 0.25,
+    "meltingPointCelsius": 1700.00,
     "stateOfMatter": "solid",
     "canBurn": False,
     "canMelt": True,
     "canFloat": False,
-    "isLooseParticle": True
+    "isLooseParticle ": True
+}
+
+iron_particle = {
+    "particleName": "sand",
+    "particle_ID": "0001",
+    "density": 7.87,
+    "strength": 72.5,
+    "meltingPointCelsius": 1538.00,
+    "stateOfMatter": "solid",
+    "canBurn": False,
+    "canMelt": True,
+    "canFloat": False,
+    "isLooseParticle": False
 }
