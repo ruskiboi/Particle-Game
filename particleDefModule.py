23d21ade610c9f11423a59e710047f4555e59de5
ruskiboi <63880257+ruskiboi@users.noreply.github.com>
2022-12-14 08:33:08 -0600

    7th-ish commit

--- particleDefModule.py
+++ particleDefModule.py
@@ -1,39 +1,90 @@
-#The strength attribute is the tensile strength in psi, divided by 1000
+# The compressiveStrength attribute is the compressive strength in MPa.
+# The tensileStrength attribute is the tensile strength in MPa.
+
+
 sand_particle = {
-    "particleName": "sand",
+
+    "particleName": "sand particle",
+    "entityType": "particle",
     "particle_ID": "0000",
     "density": 1.63,
-    "strength": 0.000129084,
+    "tensileStrengthMPa": 0,
+    "compressiveStrengthMPa": 0,
     "meltingPointCelsius": 1700.00,
     "stateOfMatter": "solid",
     "canBurn": False,
     "canMelt": True,
     "canFloat": False,
     "isLooseParticle ": True
+
 }
 
+# -- Metal Particles --
+
 iron_particle = {
-    "particleName": "sand",
+
+    "particleName": "iron particle",
+    "entityType": "particle",
     "particle_ID": "0001",
     "density": 7.87,
-    "strength": 78.3,
+    "tensileStrength": 0,
+    "compressiveStrength": 0,
     "meltingPointCelsius": 1538.00,
     "stateOfMatter": "solid",
     "canBurn": False,
     "canMelt": True,
     "canFloat": False,
     "isLooseParticle": False
+
+}
+
+steel_particle = {
+
+    "particleName": "steel particle",
+    "entityType": "particle",
+    "particle_ID": "0002",
+    "density": 7.87,
+    "tensileStrength": 0,
+    "compressiveStrength": 0,
+    "meltingPointCelsius": 1538.00,
+    "stateOfMatter": "solid",
+    "canBurn": False,
+    "canMelt": True,
+    "canFloat": False,
+    "isLooseParticle": False
+
+}
+
+aluminium_particle = {
+
+    "particleName": "aluminium particle",
+    "entityType": "particle",
+    "particle_ID": "0003",
+    "density": 2.70,
+    "tensileStrengthMPa": 90.00,
+    "compressiveStrengthMPa": 120.00,
+    "meltingPointCelsius": 658.00,
+    "stateOfMatter": "solid",
+    "canBurn": False,
+    "canMelt": True,
+    "canFloat": False,
+    "isLooseParticle": False
+
 }
 
 brick_particle = {
-    "particleName": "brick",
-    "particle_ID": "0002",
-    "density": 2.0,
-    "strength": 359.6936,
+
+    "particleName": "brick particle",
+    "entityType": "particle",
+    "particle_ID": "0000",
+    "density": 2.00,
+    "tensileStrengthMPa": 359.6936,
+    "compressiveStrengthMPa": 8.00,
     "meltingPointCelsius": 1630.00,
     "stateOfMatter": "solid",
     "canBurn": False,
     "canMelt": True,
     "canFloat": False,
     "isLooseParticle": False
+
 }
